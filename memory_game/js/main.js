328cc527396185ba32ba3c1aa1600de64f65e8d6
Mark Austin <markaustin@Marks-MacBook-Pro-2.local>
2017-11-01 00:30:53 -0700

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -7,3 +7,31 @@ var cardThree = "king";
 var cardFour = "queen";
 
 console.log("User flipped " + cardOne);
+
+
+var cards = ["queen", "queen", "king","king"];
+var cardsInPlay = [];
+
+var cardOne = cards[0];
+var cardTwo = cards[2];
+
+cardsInPlay.push(cardOne);
+cardsInPlay.push(cardTwo);
+//var c = cardsInPlay.length;
+console.log("User flipped " + cardOne);
+console.log("User flipped " + cardTwo);
+alert(cardsInPlay);
+
+
+if (cardsInPlay.length === 2){
+
+
+		if (cardsInPlay[0] === cardsInPlay[1]){
+			alert("You found a match!");
+
+		} else {
+
+			alert("Sorry, try again.");
+		}
+
+} 
